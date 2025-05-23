c5b8709d75bdd84e244972a1d3f24f2d55bcf801
goat72 <bankd8333@gmail.com>
2025-05-23 22:57:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+ const button = document.getElementById("chat");
+
+    button.addEventListener("mouseover", function() {
+      button.textContent = "شات مفعم بالحيويه ناس تتكلم في مواضيع حلوه سوالف والعاب ومجتمع نقي وراقي";
+    });
+
+    button.addEventListener("mouseout", function() {
+      button.textContent = "شات متفاعل ";
+    });
+
+ const button1 = document.getElementById("chat1");
+
+    button1.addEventListener("mouseover", function() {
+      button1.textContent = "فعاليات والعاب على كردت عشرات الملايين من الكردت تتوزع كل يوم وش منتضر ";
+    });
+
+    button1.addEventListener("mouseout", function() {
+      button1.textContent = "فعاليات بشكل يومي";
+    });
+
+     const button2 = document.getElementById("chat2");
+
+    button2.addEventListener("mouseover", function() {
+      button2.textContent = "نحرق ملايين الكردت في روم خاصه لاتفوت الفرصه لانك رح تندم والله";
+    });
+
+    button2.addEventListener("mouseout", function() {
+      button2.textContent = "حرق كردت";
+    });
+
+    
+        const button3 = document.getElementById("chat3");
+
+    button3.addEventListener("mouseover", function() {
+      button3.textContent ="سيرفر ماين كرافت جاهز من كل النواحي ادخل واستمتع";
+    });
+
+    button3.addEventListener("mouseout", function() {
+      button3.textContent ="سيرفر ماين كرافت جاهز واسطوري";
+    });
